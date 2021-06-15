cb976c1a51c47692791667c458dd9b7c5c0e8606
MBeckett <maddbeck@gmail.com>
2021-06-15 15:09:32 -0500

    Worked on steps 3 and 4, no such luck.

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -2,11 +2,13 @@ package org.launchcode.javawebdevtechjobsmvc.controllers;
 
 import org.launchcode.javawebdevtechjobsmvc.models.Job;
 import org.launchcode.javawebdevtechjobsmvc.models.JobData;
+import org.launchcode.javawebdevtechjobsmvc.models.JobField;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
 
@@ -17,20 +19,34 @@ import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.co
 @RequestMapping("search")
 public class SearchController {
 
+    private static List<Job> jobs = new ArrayList<>();
+
     @RequestMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
         return "search";
     }
+    //addAttribute is how template accesses data via the model
+//    "searchTerm" must be a string, accessed by the template using ${searchTerm}
+//    the attributeValue can be a variable of any type
+//    return specifies the path to the template
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
-    @GetMapping("search")
-    public String displaySearchResults(Model model, @RequestParam String column, @RequestParam String value){
+//    RequestParam pulls data out of incoming request
+//    ModelAttribute used for model binding. Looks at the JobData object in the POST request, sees that it
+//    has the keys and values of a JobData object.
+//    Is ModelAttribute/Model binding needed for this handler?
+//    Instructions emphasize correct annotation.
+//
 
-        if(column.toLowerCase().contains(value)){
-            model.addAttribute("search results", JobData.findByValue(value));
-            return "search/results";
-        }
-        return "redirect";
+    public String displaySearchResults(Model model, String column, String searchTerm){
+    if (searchTerm.toLowerCase().equals("all") || searchTerm.toLowerCase().equals("")){
+        jobs = JobData.findAll();
+    } else {
+        jobs = JobData.findByColumnAndValue(columnChoices.get(column), searchTerm);
+    }
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", columnChoices);
+    return "results";
     }
 }
