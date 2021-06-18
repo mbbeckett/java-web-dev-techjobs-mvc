e8aaca1a026c88692a57f3d4de0f9d975f1e5a45
MBeckett <maddbeck@gmail.com>
2021-06-17 19:29:09 -0500

    Completed Steps 1-4

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -19,6 +19,8 @@ import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.co
 @RequestMapping("search")
 public class SearchController {
 
+    ArrayList<Job> jobs = new ArrayList<>();
+
     @RequestMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
@@ -26,30 +28,17 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
-    @PostMapping("results")
-    public String displaySearchResults(Model model, @RequestParam String column, @RequestParam String searchTerm){
+
+    @PostMapping(value="results")
+    public String displaySearchResults(Model model, @ModelAttribute String column,
+                                       @ModelAttribute String value){
         ArrayList<Job> jobs;
-        if (searchTerm.toLowerCase().equals("all") || searchTerm.toLowerCase().equals("")){
+        if(column.toLowerCase().equals("all")||value.toLowerCase().equals("all")){
             jobs = JobData.findAll();
         } else {
-            jobs = JobData.findByColumnAndValue(columnChoices.get(column), searchTerm);
-            model.addAttribute("jobs", jobs);
-            model.addAttribute("column", columnChoices);
+            jobs = JobData.findByColumnAndValue(column, value);
         }
-    return "results";
+        model.addAttribute("jobs", jobs);
+        return "search";
     }
 }
-
-
-
-
-//addAttribute is how template accesses data via the model
-//    "searchTerm" must be a string, accessed by the template using ${searchTerm}
-//    the attributeValue can be a variable of any type
-//    return specifies the path to the template
-//    RequestParam pulls data out of incoming request
-//    ModelAttribute used for model binding. Looks at the JobData object in the POST request, sees that it
-//    has the keys and values of a JobData object.
-//    Is ModelAttribute/Model binding needed for this handler?
-//    Instructions emphasize correct annotation.
-//
